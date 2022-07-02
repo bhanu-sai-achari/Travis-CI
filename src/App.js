2616f5ecbcf651a28ba469bd41ba008799b3e6ff
Bhanu-sai-achari <bhanu.rymec@gmail.com>
2022-07-02 11:13:08 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>An amazing startup idea !!.</p>
+        <p>Assignment done.</p>
+        <p>Instagram: https://instagram.com/bhanusaiachari</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
